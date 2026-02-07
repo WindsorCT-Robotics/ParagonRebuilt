4ce2e7ee7a662dad23209247ab582a3a48ce513a
SuppressedCoder <aldwinpham@gmail.com>
2026-02-07 12:06:36 -0500

    Add pathToPose Command

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -1,10 +1,13 @@
 package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Radians;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.function.Supplier;
 
 import org.json.simple.parser.ParseException;
@@ -15,18 +18,26 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.ctre.phoenix6.swerve.SwerveRequest.FieldCentric;
 import com.ctre.phoenix6.swerve.SwerveRequest.FieldCentricFacingAngle;
 import com.ctre.phoenix6.swerve.SwerveRequest.RobotCentric;
-import com.ctre.phoenix6.swerve.utility.WheelForceCalculator.Feedforwards;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.path.GoalEndState;
+import com.pathplanner.lib.path.IdealStartingState;
+import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.Waypoint;
 import com.pathplanner.lib.util.DriveFeedforwards;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Dimensionless;
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
@@ -194,4 +205,47 @@ public class Drive extends GeneratedDrive {
                 }));
     }
 
+    private PathPlannerPath createPathToPosition(
+            Pose3d endPosition3d,
+            PathConstraints constraints,
+            IdealStartingState idealStartingState,
+            GoalEndState goalEndState,
+            Distance robotNextControlDistance,
+            Distance endAnchorPreviousControlDistance) {
+        SwerveDriveState robotState = getState();
+        ChassisSpeeds fieldRelativeChassisSpeeds = robotState.Speeds;
+        Angle directionOfVelocity = Radians.of(
+                Math.atan2(fieldRelativeChassisSpeeds.vyMetersPerSecond, fieldRelativeChassisSpeeds.vxMetersPerSecond));
+
+        Translation2d robotAnchor = new Translation2d(robotState.Pose.getMeasureX(), robotState.Pose.getMeasureY());
+        Translation2d robotNextControl = new Translation2d(robotNextControlDistance.in(Meters),
+                new Rotation2d(directionOfVelocity));
+        Waypoint robotPositionWaypoint = new Waypoint(new Translation2d(), robotAnchor, robotNextControl);
+
+        Pose2d endPosition2d = endPosition3d.toPose2d();
+        Translation2d endAnchor = new Translation2d(endPosition2d.getMeasureX(), endPosition2d.getMeasureY());
+        Translation2d endPreviousControl = new Translation2d(endAnchorPreviousControlDistance.in(Meters),
+                goalEndState.rotation());
+        Waypoint endPositionWaypoint = new Waypoint(endPreviousControl, endAnchor, new Translation2d());
+
+        List<Waypoint> waypoints = List.of(robotPositionWaypoint, endPositionWaypoint);
+        return new PathPlannerPath(waypoints, constraints, idealStartingState, goalEndState);
+    }
+
+    public Command pathToPosition(
+            Pose3d endPosition3d,
+            PathConstraints constraints,
+            IdealStartingState idealStartingState,
+            GoalEndState goalEndState,
+            Distance robotNextControlDistance,
+            Distance endAnchorPreviousControlDistance) {
+        return AutoBuilder.followPath(
+                createPathToPosition(
+                        endPosition3d,
+                        constraints,
+                        idealStartingState,
+                        goalEndState,
+                        robotNextControlDistance,
+                        endAnchorPreviousControlDistance));
+    }
 }

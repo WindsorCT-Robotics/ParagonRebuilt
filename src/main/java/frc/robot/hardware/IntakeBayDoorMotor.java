6eb8a2634ccf5238dd60f4e64a1446d7c40f24f1
SuppressedCoder <aldwinpham@gmail.com>
2026-02-09 23:51:25 -0500

    Start IntakeBayDoorMotor implementation

---
+++ src/main/java/frc/robot/hardware/IntakeBayDoorMotor.java
@@ -0,0 +1,14 @@
+package frc.robot.hardware;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+public class IntakeBayDoorMotor { // TODO: Extend IMotor once it's merged into main.
+    private final SparkMax motor;
+    private final SparkMaxConfig configuration;
+
+    // TODO: When CanID is merged into main add it.
+    public IntakeBayDoorMotor() {
+
+    }
+}

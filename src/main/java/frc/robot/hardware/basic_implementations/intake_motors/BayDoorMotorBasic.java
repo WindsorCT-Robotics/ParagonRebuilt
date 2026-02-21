82eeb1bb87c2c59dcb59f31b10870ad17da2caa7
RoboticsIsEpik <102563278+SuppressedCoder@users.noreply.github.com>
2026-02-20 23:09:24 -0500

    Remove old BayDoorMotorBasic with new

--- src/main/java/frc/robot/hardware/basic_implementations/intake_motors/BayDoorMotorBasic.java
+++ src/main/java/frc/robot/hardware/basic_implementations/intake_motors/BayDoorMotorBasic.java
@@ -2,7 +2,6 @@ package frc.robot.hardware.basic_implementations.intake_motors;
 
 import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Percent;
-import static edu.wpi.first.units.Units.RPM;
 import static edu.wpi.first.units.Units.Rotations;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 import static edu.wpi.first.units.Units.Volts;
@@ -17,64 +16,50 @@ import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Dimensionless;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.util.sendable.SendableBuilder;
 import frc.robot.hardware.CanId;
 import frc.robot.hardware.base_motors.NeoMotorBase;
-import frc.robot.interfaces.IAngularPositionMotor;
 
-public class BayDoorMotorBasic extends NeoMotorBase {
-    private static final IdleMode IDLE_MODE = IdleMode.kBrake;
-    private static final boolean INVERTED = false;
-    private static final Current CURRENT_LIMIT = DEFAULT_CURRENT;
-    private static final ResetMode RESET_MODE = ResetMode.kResetSafeParameters;
-    private static final PersistMode PERSIST_MODE = PersistMode.kPersistParameters;
-    private static final AngularVelocity POSITION_ANGULAR_VELOCITY = RPM.of(1);
+public class BayDoorMotorBasic2 extends NeoMotorBase {
+    private static final Current STRUGGLE_THRESHOLD = Amps.of(40); // TODO: Test value
+    private static final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(0.1, 0); // TODO: Figure
+                                                                                                  // velocity (kV)
 
-    private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(0.1, 0); // TODO: Figure velocity (kV)
+    private static final AngularVelocity MAX_ANGULAR_VELOCITY = RotationsPerSecond.of(1); // TODO: Figure good speed.
+    // These are zero because the Bay Door should only be controlled by setting the
+    // rotations per second.
+    private static final Voltage MAX_VOLTAGE = Volts.of(0);
+    private static final Dimensionless MAX_PERCENTAGE = Percent.of(0);
 
     public static final Angle OPEN_ANGLE = Rotations.of(21.5);
     public static final Angle CLOSE_ANGLE = Rotations.of(0);
 
     private BayDoorState motorBayDoorState = BayDoorState.UNKNOWN;
 
-    // TODO: Put caps on speed.
-    public BayDoorMotorBasic(String name, CanId canId) {
-        super(
-                name,
-                canId,
-                new SparkMaxConfig()
-                        .idleMode(IDLE_MODE)
-                        .inverted(INVERTED)
-                        .smartCurrentLimit((int) CURRENT_LIMIT.in(Amps)),
-                RESET_MODE,
-                PERSIST_MODE);
+    public BayDoorMotorBasic2(String name, CanId canId) {
+        super(name, canId, feedforward,
+                new SparkMaxConfig().idleMode(IdleMode.kBrake).inverted(false).smartCurrentLimit(
+                        (int) NeoMotorBase.DEFAULT_CURRENT.in(Amps)),
+                ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters,
+                MAX_ANGULAR_VELOCITY, MAX_VOLTAGE, MAX_PERCENTAGE);
     }
 
-    public Dimensionless getDutyCycle() {
-        return Percent.of(motor.get());
-    }
-
-    public void setIdleMode(IdleMode idleMode) {
-        motorConfiguration.idleMode(idleMode);
-    }
-
-    private Angle getMotorPosition() {
-        return Rotations.of(motor.getEncoder().getPosition());
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        builder.addStringProperty("Bay Motor State", () -> getBayMotorState().toString(), null);
+        builder.addBooleanProperty("Soft Forward Limit", this::atSoftForwardLimit, null);
+        builder.addBooleanProperty("Soft Reverse Limit", this::atSoftReverseLimit, null);
+        builder.addBooleanProperty("Is Moving", this::isMoving, null);
+        builder.addBooleanProperty("Is Struggling", this::isStruggling, null);
     }
 
     public boolean atSoftForwardLimit() {
-        return getMotorPosition().gte(OPEN_ANGLE);
+        return getAngle().gte(OPEN_ANGLE);
     }
 
     public boolean atSoftReverseLimit() {
-        return getMotorPosition().lte(CLOSE_ANGLE);
-    }
-
-    public AngularVelocity getVelocity() {
-        return RPM.of(motor.getEncoder().getVelocity());
-    }
-
-    public void setRPS(AngularVelocity velocity) {
-        setVoltage(Volts.of(feedforward.calculate(velocity.in(RotationsPerSecond))));
+        return getAngle().lte(CLOSE_ANGLE);
     }
 
     public BayDoorState getBayMotorState() {
@@ -83,5 +68,9 @@ public class BayDoorMotorBasic extends NeoMotorBase {
 
     public void setBayMotorState(BayDoorState state) {
         motorBayDoorState = state;
+    }
+
+    public boolean isStruggling() {
+        return getCurrent().gt(STRUGGLE_THRESHOLD);
     }
 }

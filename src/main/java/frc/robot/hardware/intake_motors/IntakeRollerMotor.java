fba8b0d919616d9f8bf70b7260d08b9b02216fd8
RoboticsIsEpik <102563278+SuppressedCoder@users.noreply.github.com>
2026-02-21 08:13:30 -0500

    Convert the rest of motors to work with new motor bases

--- src/main/java/frc/robot/hardware/intake_motors/IntakeRollerMotor.java
+++ src/main/java/frc/robot/hardware/intake_motors/IntakeRollerMotor.java
@@ -1,32 +1,35 @@
 package frc.robot.hardware.intake_motors;
 
 import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Percent;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Volts;
 
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
-import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Dimensionless;
+import edu.wpi.first.units.measure.Voltage;
 import frc.robot.hardware.CanId;
 import frc.robot.hardware.base_motors.NeoMotorBase;
 
 public class IntakeRollerMotor extends NeoMotorBase {
-    private static final IdleMode IDLE_MODE = IdleMode.kBrake;
-    private static final ResetMode RESET_MODE = ResetMode.kResetSafeParameters;
-    private static final PersistMode PERSIST_MODE = PersistMode.kPersistParameters;
-    private static final boolean INVERTED = false;
-    private static final Current CURRENT_LIMIT = DEFAULT_CURRENT;
+    private static final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(0.1, 0); // TODO: Figure
+    private static final AngularVelocity MAX_ANGULAR_VELOCITY = RotationsPerSecond.of(1); // TODO: Figure good speed.
+    // These are zero because the Bay Door should only be controlled by setting the
+    // rotations per second.
+    private static final Voltage MAX_VOLTAGE = Volts.of(0);
+    private static final Dimensionless MAX_PERCENTAGE = Percent.of(0);
 
     public IntakeRollerMotor(String name, CanId canId) {
-        super(
-                name,
-                canId,
-                new SparkMaxConfig()
-                        .idleMode(IDLE_MODE)
-                        .inverted(INVERTED)
-                        .smartCurrentLimit((int) CURRENT_LIMIT.in(Amps)),
-                RESET_MODE,
-                PERSIST_MODE);
+        super(name, canId, feedforward,
+                new SparkMaxConfig().idleMode(IdleMode.kBrake).inverted(false).smartCurrentLimit(
+                        (int) DEFAULT_CURRENT.in(Amps)),
+                ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters,
+                MAX_ANGULAR_VELOCITY, MAX_VOLTAGE, MAX_PERCENTAGE);
     }
 }

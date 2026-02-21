637a4be32dd8f47db7f877d6ef8f4195415be2ca
RoboticsIsEpik <102563278+SuppressedCoder@users.noreply.github.com>
2026-02-20 22:09:27 -0500

    Remove old Motor Bases

--- src/main/java/frc/robot/hardware/base_motors/SparkMaxMotorBase.java
+++ src/main/java/frc/robot/hardware/base_motors/SparkMaxMotorBase.java
@@ -6,16 +6,20 @@ import static edu.wpi.first.units.Units.Percent;
 import static edu.wpi.first.units.Units.RPM;
 import static edu.wpi.first.units.Units.Rotations;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Value;
 import static edu.wpi.first.units.Units.Volts;
 
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkBaseConfig;
+import java.util.function.Consumer;
+
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfigAccessor;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
@@ -28,86 +32,82 @@ import edu.wpi.first.util.sendable.SendableRegistry;
 import frc.robot.hardware.CanId;
 import frc.robot.interfaces.IMotor;
 
-public abstract class SparkMaxMotorBase implements IMotor, Sendable {
-    protected final SparkMax motor;
-    protected final SparkBaseConfig motorConfiguration;
-    protected final SparkClosedLoopController closedLoopController;
-    protected final CanId canId;
+public class SparkMaxMotorBase implements IMotor<SparkMax, SparkMaxConfigAccessor>, Sendable {
+    private final SparkMax motor;
+    private final SimpleMotorFeedforward feedforward;
     private final AngularVelocity maxAngularVelocity;
+    private final Voltage maxVoltage;
+    private final Dimensionless maxPercentage;
 
-    private static final Dimensionless MAX_DUTY = Percent.of(100);
-    private static final Dimensionless MIN_DUTY = Percent.of(-100);
-
-    private final ResetMode resetMode;
-    private final PersistMode persistMode;
-
-    /**
-     * 
-     * @param name
-     * @param canId
-     * @param motorType
-     * @param maxAngularVelocity
-     * @param motorConfiguration
-     * @param resetMode          It's recommended that when initalized, it should be
-     *                           set to `kResetSafeParameters`
-     * @param persistMode        It's recommended that when initalized, it should be
-     *                           set to `kPersistParameters`
-     */
     protected SparkMaxMotorBase(
             String name,
             CanId canId,
             MotorType motorType,
-            AngularVelocity maxAngularVelocity,
-            SparkBaseConfig motorConfiguration,
+            SimpleMotorFeedforward feedforward,
+            SparkBaseConfig configuration,
             ResetMode resetMode,
-            PersistMode persistMode) {
+            PersistMode persistMode,
+            AngularVelocity maxAngularVelocity,
+            Voltage maxVoltage,
+            Dimensionless maxPercentage) {
         SendableRegistry.add(this, name);
-        this.canId = canId;
-        this.maxAngularVelocity = maxAngularVelocity;
-        this.motorConfiguration = motorConfiguration;
         motor = new SparkMax(canId.Id(), motorType);
-        motor.configure(this.motorConfiguration, resetMode, persistMode);
-        closedLoopController = motor.getClosedLoopController();
-        this.resetMode = resetMode;
-        this.persistMode = persistMode;
+        motor.configure(configuration, resetMode, persistMode);
+        this.feedforward = feedforward;
+        this.maxAngularVelocity = maxAngularVelocity;
+        this.maxVoltage = maxVoltage;
+        this.maxPercentage = maxPercentage;
     }
 
     @Override
-    public void initSendable(SendableBuilder builder) {
-        builder.setActuator(true);
-        builder.setSafeState(this::stop);
-
-        // builder.addDoubleProperty("Voltage (V)", () -> getVoltage().in(Volts),
-        // this::setVoltage);
-        // builder.addDoubleProperty("Current (Amps)", () -> getCurrent().in(Amps),
-        // null);
-        // builder.addBooleanProperty("Is Motor Moving?", this::isMoving, null);
-        // builder.addDoubleProperty("Target Duty Cycle %", this::getDutyCycle,
-        // this::setDutyCycle);
-        // builder.addDoubleProperty("RPM (Rotations Per Minute)", () ->
-        // getAngularVelocity().in(RPM),
-        // this::setRPM);
-        // builder.addDoubleProperty("Temperature (C)", () ->
-        // getTemperature().in(Celsius), null);
+    public void configure(Consumer<SparkMax> config) {
+        config.accept(motor);
     }
 
     @Override
-    public void stop() {
-        motor.stopMotor();
+    public Angle getAngle() {
+        return Rotations.of(motor.getEncoder().getPosition());
+    }
+
+    // TODO: Possibly add this to telemetry?
+    @Override
+    public SparkMaxConfigAccessor getConfiguration() {
+        return motor.configAccessor;
     }
 
     @Override
-    public void resetRelativeEncoder() {
-        motor.getEncoder().setPosition(0);
+    public Dimensionless getDutyCycle() {
+        return Value.of(motor.getAppliedOutput());
+    }
+
+    /**
+     * Duty Cycles are proportional to battery life.
+     */
+    @Override
+    public void setDutyCycle(Dimensionless percentage) {
+        Dimensionless clampedPercentage = Percent
+                .of(MathUtil.clamp(percentage.in(Percent), maxPercentage.unaryMinus().in(Percent),
+                        maxPercentage.in(Percent)));
+        motor.set(clampedPercentage.in(Percent));
     }
 
     @Override
-    public void setVoltage(Voltage voltage) {
-        motor.setVoltage(voltage.in(Volts));
+    public AngularVelocity getVelocity() {
+        return RPM.of(motor.getEncoder().getVelocity());
     }
 
-    private void setVoltage(double voltage) {
-        motor.setVoltage(voltage);
+    @Override
+    public void setRPS(AngularVelocity velocity) {
+        AngularVelocity clampedVelocity = RotationsPerSecond.of(
+                MathUtil.clamp(
+                        velocity.in(RotationsPerSecond),
+                        maxAngularVelocity.unaryMinus().in(RotationsPerSecond),
+                        maxAngularVelocity.in(RotationsPerSecond)));
+
+        setVoltage(
+                Volts.of(
+                        feedforward.calculate(
+                                clampedVelocity.in(RotationsPerSecond))));
     }
 
     @Override
@@ -116,51 +116,49 @@ public abstract class SparkMaxMotorBase implements IMotor, Sendable {
     }
 
     @Override
-    public boolean isMoving() {
-        return (motor.getEncoder().getVelocity() != 0);
+    public void setVoltage(Voltage voltage) {
+        Voltage clampedVoltage = Volts
+                .of(MathUtil.clamp(voltage.in(Volts), maxVoltage.unaryMinus().in(Volts), maxVoltage.in(Volts)));
+        motor.setVoltage(clampedVoltage.in(Volts));
     }
 
     @Override
-    public void setDutyCycle(Dimensionless percentage) {
-        if (percentage.gt(MAX_DUTY) || percentage.lt(MIN_DUTY)) {
-            throw new IllegalArgumentException("Percentage " + percentage
-                    + " is out of bounds for duty motor Acceptable ranges are [" + MIN_DUTY + ", " + MAX_DUTY + "].");
-        }
-
-        motor.set(percentage.in(Percent));
+    public boolean isMoving() {
+        return motor.getEncoder().getVelocity() != 0;
     }
 
-    private void setDutyCycle(double percentage) {
-        double clampPercentage = MathUtil.clamp(percentage, MIN_DUTY.in(Percent), MAX_DUTY.in(Percent));
-        setDutyCycle(Percent.of(clampPercentage));
+    @Override
+    public void resetRelativeEncoder() {
+        motor.getEncoder().setPosition(0);
+
     }
 
-    private double getDutyCycle() {
-        return motor.getAppliedOutput();
-    }
-
-    private AngularVelocity getAngularVelocity() {
-        return RPM.of(motor.getEncoder().getVelocity());
-    }
-
-    private Temperature getTemperature() {
-        return Celsius.of(motor.getMotorTemperature());
-    }
-
-    private Current getCurrent() {
+    @Override
+    public Current getCurrent() {
         return Amps.of(motor.getOutputCurrent());
     }
 
-    public Angle getRotation() {
-        return Rotations.of(motor.getEncoder().getPosition());
+    @Override
+    public Temperature getTemperarure() {
+        return Celsius.of(motor.getMotorTemperature());
     }
 
-    public void setInverted(boolean inverted) {
-        motorConfiguration.inverted(inverted);
-        motor.configure(motorConfiguration, resetMode, persistMode);
+    @Override
+    public void stop() {
+        motor.stopMotor();
     }
 
-    public boolean getInversion() {
-        return motor.configAccessor.getInverted();
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        builder.setActuator(true);
+        builder.setSafeState(this::stop);
+
+        builder.addDoubleProperty("Angle (Rotations)", () -> getAngle().in(Rotations), null);
+        builder.addDoubleProperty("Duty Cycle (%)", () -> getDutyCycle().in(Percent), null);
+        builder.addDoubleProperty("Velocity (RPS)", () -> getVelocity().in(RotationsPerSecond), null);
+        builder.addBooleanProperty("Is Moving", this::isMoving, null);
+        builder.addDoubleProperty("Voltage (V)", () -> getVoltage().in(Volts), null);
+        builder.addDoubleProperty("Current (Amps)", () -> getCurrent().in(Amps), null);
+        builder.addDoubleProperty("Temperature (C)", () -> getTemperarure().in(Celsius), null);
     }
 }
--- src/main/java/frc/robot/hardware/base_motors/TalonFXMotorBase.java
+++ src/main/java/frc/robot/hardware/base_motors/TalonFXMotorBase.java
@@ -4,19 +4,19 @@ import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Celsius;
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Percent;
-import static edu.wpi.first.units.Units.RPM;
-import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
 import static edu.wpi.first.units.Units.Volts;
 
-import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import java.util.function.Consumer;
+
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Dimensionless;
@@ -24,50 +24,87 @@ import edu.wpi.first.units.measure.Temperature;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
-import frc.robot.interfaces.IMotor;
+import edu.wpi.first.util.sendable.SendableRegistry;
 import frc.robot.hardware.CanId;
+import frc.robot.interfaces.IMotor;
 
-public abstract class TalonFXMotorBase implements IMotor, Sendable {
-    // https://v6.docs.ctr-electronics.com/en/stable/docs/api-reference/api-usage/configuration.html
-    protected final TalonFX motor;
-    protected final TalonFXConfigurator motorConfigurator;
+public class TalonFXMotorBase implements IMotor<TalonFX, TalonFXConfiguration>, Sendable {
+    private final TalonFX motor;
+    private final TalonFXConfigurator configurator;
+    private final TalonFXConfiguration configuration;
+    private final SimpleMotorFeedforward feedforward;
     private final AngularVelocity maxAngularVelocity;
+    private final Voltage maxVoltage;
+    private final Dimensionless maxPercentage;
 
-    protected TalonFXMotorBase(
+    public TalonFXMotorBase(
             String name,
             CanId canId,
-            NeutralModeValue neutralMode,
-            InvertedValue inverted,
-            Current currentLimit,
-            AngularVelocity maxAngularVelocity) {
+            TalonFXConfiguration configuration,
+            SimpleMotorFeedforward feedforward,
+            AngularVelocity maxAngularVelocity,
+            Voltage maxVoltage,
+            Dimensionless maxPercentage) {
+        SendableRegistry.add(this, name);
         motor = new TalonFX(canId.Id());
-        motorConfigurator = motor.getConfigurator();
-        TalonFXConfiguration motorConfiguration = new TalonFXConfiguration()
-                .withMotorOutput(
-                        new MotorOutputConfigs()
-                                .withNeutralMode(neutralMode)
-                                .withInverted(inverted))
-                .withCurrentLimits(
-                        new CurrentLimitsConfigs()
-                                .withStatorCurrentLimit(currentLimit)
-                                .withStatorCurrentLimitEnable(true));
-        motorConfigurator.apply(motorConfiguration);
+        configurator = motor.getConfigurator();
+        this.configuration = configuration;
+        configurator.apply(configuration);
+        this.feedforward = feedforward;
         this.maxAngularVelocity = maxAngularVelocity;
+        this.maxVoltage = maxVoltage;
+        this.maxPercentage = maxPercentage;
     }
 
     @Override
-    public void initSendable(SendableBuilder builder) {
-        builder.setActuator(true);
-        builder.setSafeState(this::stop);
+    public void configure(Consumer<TalonFX> config) {
+        config.accept(motor);
+    }
 
-        builder.addDoubleProperty("Voltage (V)", () -> getVoltage().in(Volts), this::setVoltage);
-        builder.addDoubleProperty("Current (Amps)", () -> getCurrent().in(Amps), null);
-        builder.addBooleanProperty("Is Motor Moving?", this::isMoving, null);
-        builder.addDoubleProperty("Target Duty Cycle %", this::getDutyCycle, this::setDutyCycle);
-        builder.addDoubleProperty("RPM (Rotations Per Minute)", () -> getAngularVelocity().in(RPM),
-                this::setRPM);
-        builder.addDoubleProperty("Temperature (C)", () -> getTemperature().in(Celsius), null);
+    @Override
+    public Angle getAngle() {
+        return motor.getPosition().getValue();
+    }
 
+    // TODO: Possibly add this to telemetry?
+    @Override
+    public TalonFXConfiguration getConfiguration() {
+        return configuration;
+    }
+
+    @Override
+    public Dimensionless getDutyCycle() {
+        return Percent.of(motor.getDutyCycle().getValue());
+    }
+
+    /**
+     * Duty Cycles are proportional to battery life.
+     */
+    @Override
+    public void setDutyCycle(Dimensionless percentage) {
+        Dimensionless clampedPercentage = Percent
+                .of(MathUtil.clamp(percentage.in(Percent), maxPercentage.unaryMinus().in(Percent),
+                        maxPercentage.in(Percent)));
+        motor.set(clampedPercentage.in(Percent));
+    }
+
+    @Override
+    public AngularVelocity getVelocity() {
+        return motor.getVelocity().getValue();
+    }
+
+    @Override
+    public void setRPS(AngularVelocity velocity) {
+        AngularVelocity clampedVelocity = RotationsPerSecond.of(
+                MathUtil.clamp(
+                        velocity.in(RotationsPerSecond),
+                        maxAngularVelocity.unaryMinus().in(RotationsPerSecond),
+                        maxAngularVelocity.in(RotationsPerSecond)));
+
+        setVoltage(
+                Volts.of(
+                        feedforward.calculate(
+                                clampedVelocity.in(RotationsPerSecond))));
     }
 
     @Override
@@ -75,40 +112,31 @@ public abstract class TalonFXMotorBase implements IMotor, Sendable {
         return motor.getMotorVoltage().getValue();
     }
 
-    public void setVoltate(Voltage voltage) {
-        motor.setVoltage(voltage.in(Volts));
-    }
-
-    public void setVoltage(double voltage) {
-        motor.setVoltage(voltage);
+    @Override
+    public void setVoltage(Voltage voltage) {
+        Voltage clampedVoltage = Volts
+                .of(MathUtil.clamp(voltage.in(Volts), maxVoltage.unaryMinus().in(Volts), maxVoltage.in(Volts)));
+        motor.setVoltage(clampedVoltage.in(Volts));
     }
 
     @Override
     public boolean isMoving() {
-        return !motor.getAcceleration().getValue().equals(RotationsPerSecondPerSecond.of(0));
+        return motor.getVelocity().getValueAsDouble() != 0;
     }
 
     @Override
     public void resetRelativeEncoder() {
-        motor.setPosition(Degrees.of(0));
+        motor.setPosition(Degrees.zero());
     }
 
     @Override
-    public void setDutyCycle(Dimensionless percentage) {
-        motor.set(MathUtil.clamp(percentage.in(Percent), -1, 1));
-    }
-
-    private void setDutyCycle(double percentage) {
-        motor.set(MathUtil.clamp(percentage, -1, 1));
-    }
-
-    private double getDutyCycle() {
-        return motor.getDutyCycle().getValue();
+    public Current getCurrent() {
+        return motor.getTorqueCurrent().getValue();
     }
 
     @Override
-    public void setVoltage(Voltage voltage) {
-        motor.setVoltage(voltage.in(Volts));
+    public Temperature getTemperarure() {
+        return motor.getDeviceTemp().getValue();
     }
 
     @Override
@@ -116,19 +144,17 @@ public abstract class TalonFXMotorBase implements IMotor, Sendable {
         motor.stopMotor();
     }
 
-    public Current getCurrent() {
-        return motor.getTorqueCurrent().getValue();
-    }
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        builder.setActuator(true);
+        builder.setSafeState(this::stop);
 
-    public void setRPM(double rpm) {
-        motor.set(rpm / maxAngularVelocity.in(RPM));
-    }
-
-    public AngularVelocity getAngularVelocity() {
-        return motor.getVelocity().getValue();
-    }
-
-    private Temperature getTemperature() {
-        return motor.getDeviceTemp().getValue();
+        builder.addDoubleProperty("Angle (Rotations)", () -> getAngle().in(Rotations), null);
+        builder.addDoubleProperty("Duty Cycle (%)", () -> getDutyCycle().in(Percent), null);
+        builder.addDoubleProperty("Velocity (RPS)", () -> getVelocity().in(RotationsPerSecond), null);
+        builder.addBooleanProperty("Is Moving", this::isMoving, null);
+        builder.addDoubleProperty("Voltage (V)", () -> getVoltage().in(Volts), null);
+        builder.addDoubleProperty("Current (Amps)", () -> getCurrent().in(Amps), null);
+        builder.addDoubleProperty("Temperature (C)", () -> getTemperarure().in(Celsius), null);
     }
 }

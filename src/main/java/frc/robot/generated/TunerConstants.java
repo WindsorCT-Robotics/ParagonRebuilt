82f8bcdd24189ed148407fa99b7763b90b36ef32
SuppressedCoder <102563278+SuppressedCoder@users.noreply.github.com>
2026-03-28 08:59:56 -0400

    drive motors to 80 apms

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -61,7 +61,7 @@ public class TunerConstants {
         private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration()
                         .withCurrentLimits(
                                         new CurrentLimitsConfigs()
-                                                        .withStatorCurrentLimit(Amps.of(100)));
+                                                        .withStatorCurrentLimit(Amps.of(80)));
         private static final TalonFXConfiguration steerInitialConfigs = new TalonFXConfiguration()
                         .withCurrentLimits(
                                         new CurrentLimitsConfigs()

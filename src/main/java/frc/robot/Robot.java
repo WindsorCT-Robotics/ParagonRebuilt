eac8fc53d20da21528d4a42119059cad8c27bba3
SuppressedCoder <102563278+SuppressedCoder@users.noreply.github.com>
2026-04-09 20:41:16 -0400

    Add hub util

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,10 +11,12 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.utils.HubUtil;
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
   private final RobotContainer m_robotContainer;
+  private final HubUtil hubUtil = HubUtil.getInstance();
 
   public Robot() {
     SignalLogger.enableAutoLogging(false);
@@ -27,6 +29,7 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
     m_robotContainer.updateLaunchParameters();
+    hubUtil.periodic();
   }
 
   @Override
--- src/main/java/frc/robot/utils/HubUtil.java
+++ src/main/java/frc/robot/utils/HubUtil.java
@@ -1,33 +1,132 @@
 package frc.robot.utils;
 
-import edu.wpi.first.units.measure.Time;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
-// Reference: https://github.com/Mechanical-Advantage/RobotCode2026Public/blob/main/src%2Fmain%2Fjava%2Forg%2Flittletonrobotics%2Ffrc2026%2Futil%2FHubShiftUtil.java
 public class HubUtil {
-    public enum GameState {
-        UNKNOWN,
-        AUTONOMOUS,
-        TRANSITION,
-        TELEOPERATED,
-        ENDGAME
+    private HubUtil(){}
+
+    static HubUtil _inst = new HubUtil();
+    public static HubUtil getInstance(){ return _inst; }
+
+    private class TimeSegment {
+        public double start;
+        public double end;
+        public TimeSegment(double start, double end) {
+            this.start = start;
+            this.end = end;
+        }
+        public boolean isTimeWithin(double time) {
+            return time >= start && time <= end;
+        }
+    }
+    private final TimeSegment[] TeleopHubActiveTimesForAutoWinner = new TimeSegment[] {
+        new TimeSegment(0, 55), // Shift 4 combines with End game
+        new TimeSegment((1 * 60) + 20, (1 * 60) + 45), // Shift 2
+        new TimeSegment((2 * 60) + 10, (2 * 60) + 20) // Transition Shift
+    };
+    private final TimeSegment[] TeleopHubActiveTimesForAutoLoser = new TimeSegment[] {
+        new TimeSegment(0, 30), // End game
+        new TimeSegment(55, (1 * 60) + 20), // Shift 3
+        new TimeSegment((1 * 60) + 45, (2 * 60) + 20) // Shift 1 combines with transition shift
+    };
+
+    /* True when our hub is active */
+    private boolean isHubActive = false;
+    private double timeUntilTransition = 0;
+    
+    /* What to publish over networktables for telemetry */
+    private final NetworkTable inst = NetworkTableInstance.getDefault().getTable("Hub");
+    private final BooleanPublisher isHubActivePublisher = inst.getBooleanTopic("Active").publish();
+    private final DoublePublisher timeUntilSwapPublisher = inst.getDoubleTopic("Time Until Swap").publish();
+
+    private void updateStatesForTeleop() {
+        if (!DriverStation.isTeleopEnabled()) return;
+
+        String whoWonAuto = DriverStation.getGameSpecificMessage();
+        boolean redIsWinner;
+        switch (whoWonAuto) {
+            case "R": redIsWinner = true; break;
+            case "B": redIsWinner = false; break;
+            default: return; // We don't have the message yet, we can't determine
+        }
+
+        /* Check to see if our current time is within a time that it doesn't matter what alliance we're on */
+        double timeLeftInTeleop = DriverStation.getMatchTime();
+        double timeUntilSwap = 150; // Start at 2:30 so it definitely gets cleared
+
+        /* When we're in teleop we should definitely have the alliance color, so we can confidently use the DS alliance API */
+        boolean isRedAlliance = DriverStation.getAlliance().orElse(Alliance.Red) == Alliance.Red;
+
+        /* This boils down to an XOR but we explicitly call out both cases so it's clear */
+        boolean useWinnerTimes = (redIsWinner && isRedAlliance) || (!redIsWinner && !isRedAlliance);
+
+        /* If we're the winner, use the winner times */
+        if (useWinnerTimes) {
+            for (TimeSegment seg : TeleopHubActiveTimesForAutoWinner) {
+                if (seg.isTimeWithin(timeLeftInTeleop)) {
+                    timeUntilTransition = timeLeftInTeleop - seg.start;
+                    isHubActive = true;
+                    return;
+                }
+                double timeToStart = timeLeftInTeleop - seg.end;
+                if (timeToStart > 0 && timeToStart < timeUntilSwap) {
+                    /* Update our time until swap with this, since it's sooner */
+                    timeUntilSwap = timeToStart;
+                }
+            }
+        } else {
+            for (TimeSegment seg : TeleopHubActiveTimesForAutoLoser) {
+                if (seg.isTimeWithin(timeLeftInTeleop)) {
+                    timeUntilTransition = timeLeftInTeleop - seg.start;
+                    isHubActive = true;
+                    return;
+                }
+                double timeToStart = timeLeftInTeleop - seg.end;
+                if (timeToStart > 0 && timeToStart < timeUntilSwap) {
+                    /* Update our time until swap with this, since it's sooner */
+                    timeUntilSwap = timeToStart;
+                }
+            }
+        }
+        timeUntilTransition = timeUntilSwap;
+        isHubActive = false;
     }
 
-    public enum TeleOperatedState {
-        UNKNOWN,
-        RED_HUB_INACTIVE,
-        RED_HUB_ACTIVE,
-        BLUE_HUB_INACTIVE,
-        BLUE_HUB_ACTIVE,
+    private void fetchInputs() {
+        /* Check what state we're in */
+        if (DriverStation.isDisabled()) {
+            /* If we're disabled, the hub is always inactive */
+            isHubActive = false;
+            timeUntilTransition = -1;
+        } else if (DriverStation.isAutonomous()) {
+            /* If we're autonomous, the hub is always active */
+            isHubActive = true;
+            timeUntilTransition = DriverStation.getMatchTime();
+        } else if (DriverStation.isTeleop()) {
+            /* If we're teleop, the hub will go through phases as outlined in the manual that we'll use to determine the active state */
+            updateStatesForTeleop();
+        } else {
+            /* This is a state that we don't recognize, so make ti false */
+            isHubActive = false;
+        }
     }
 
-    public record HubInfo(GameState gameState, TeleOperatedState hubState, Time untilInactive) {
+    public void periodic() {
+        fetchInputs();
+        isHubActivePublisher.accept(isHubActive);
+        timeUntilSwapPublisher.accept(timeUntilTransition);
     }
 
-    private static Timer timer = new Timer();
+    public boolean isOurHubActive() {
+        return isHubActive;
+    }
 
-    // TODO: Properly implement HubUtil.
-    // public static Alliance getHubActiveAlliance() {
-
-    // }
+    public double timeUntilTransition() {
+        return timeUntilTransition;
+    }
 }

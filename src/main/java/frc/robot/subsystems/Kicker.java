cffbfcf99bf65de1360afebdf47e2356dd3406a7
SuppressedCoder <102563278+SuppressedCoder@users.noreply.github.com>
2026-04-29 17:19:11 -0400

    Update manual defaults

--- src/main/java/frc/robot/subsystems/Kicker.java
+++ src/main/java/frc/robot/subsystems/Kicker.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.RPM;
-import static edu.wpi.first.units.Units.RotationsPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
 import static edu.wpi.first.units.Units.Watts;
 
@@ -40,7 +39,7 @@ public class Kicker extends SubsystemBase {
                             .withKV(0.01)));
 
     private static final AngularVelocity PREP_ANGULAR_VELOCITY = RPM.of(1500);
-    private AngularVelocity kickVelocity = RotationsPerSecond.of(0);
+    private AngularVelocity kickVelocity = RPM.of(2000);
 
     public Kicker(String name) {
         super("Subsystems/" + name);
